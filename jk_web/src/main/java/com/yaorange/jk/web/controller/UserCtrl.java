ef131d63ab8c83305e27a9a713134ee0c20a9d6f
tamsiuloong <327395128@qq.com>
2018-03-12 22:11:59 +0800

    spring security 获取当前数据库认证用户信息

--- jk_web/src/main/java/com/yaorange/jk/web/controller/UserCtrl.java
+++ jk_web/src/main/java/com/yaorange/jk/web/controller/UserCtrl.java
@@ -4,6 +4,8 @@ import com.yaorange.jk.entity.User;
 import com.yaorange.jk.entity.vo.ApiResponse;
 import com.yaorange.jk.utils.ResponseCode;
 import com.yaorange.jk.utils.SysConstant;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,13 +30,14 @@ public class UserCtrl {
 //        {
 //            return new ApiResponse(SysConstant.API_MESSAGE_PARAM_ERROR,"500",ResponseCode.API_CODE_CALL_FAIL);
 //        }
-
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication() .getPrincipal();
         User user = new User();
         user.setId("1");
         user.setUid("1");
-        user.setName("admin");
+        user.setName(userDetails.getUsername());
         user.setIntroduction("im super pig");
         List<String>  permissions = new ArrayList<>();
+        userDetails.getAuthorities().forEach(a->{permissions.add(a.getAuthority());});
         permissions.add("admin");
 
         user.setPermissions(permissions);
